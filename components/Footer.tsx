903ca7fbb95a1980dd013fa1a88fcece1550ec04
OtaraAlex <mbechealex61@gmail.com>
2023-06-26 16:36:34 +0300

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,23 +31,22 @@ const Footer = () => {
             </div>
           ))}
         </div>
-        </div>
+      </div>
 
-        <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-            <p>@2023 CarHub. All Rights Reserved</p>
+      <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+        <p>@2023 CarHub. All rights reserved</p>
 
-          <div className="foooter__copyrights-link">
-              <Link href="/" className="text-gray-500">
-                   Privacy Policy
-              </Link>
-                 |
-              <Link href="/" className="text-gray-500">
-                   Terms of Use
-              </Link>
-          </div>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms & Conditions
+          </Link>
         </div>
+      </div>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
