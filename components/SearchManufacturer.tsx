0f257b0774fa998f0f3ef5fdaca00b29b775e98f
OtaraAlex <mbechealex61@gmail.com>
2023-09-25 02:09:13 +0300

    Apply higher z-index for smaller screens in SearchManufacturer dropdown menu

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -52,7 +52,7 @@ const SearchManufacturer = ({ selected, setSelected }: SearchManufacturerProps) 
             afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
             <Combobox.Options
-              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50'
               static
             >
               {filteredManufacturers.length === 0 && query !== "" ? (
@@ -66,7 +66,7 @@ const SearchManufacturer = ({ selected, setSelected }: SearchManufacturerProps) 
                 filteredManufacturers.map((item) => (
                   <Combobox.Option
                     key={item}
-                    className={({ active }) =>
+                    className={({ active }) => 
                       `relative search-manufacturer__option ${
                         active ? "bg-primary-blue text-white" : "text-gray-900"
                       }`
