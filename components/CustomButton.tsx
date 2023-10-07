bc6ff995b514860ad4fa4bd2960c867bbacf05cd
OtaraAlex <mbechealex61@gmail.com>
2023-10-07 17:31:57 +0300

    Update CarCard

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 import { CustomButtonProps } from "@/types";
 
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon, isDisabled }: CustomButtonProps) => {
   return (
     <button
-      disabled={false}
+      disabled={isDisabled}
       type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
